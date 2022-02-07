2258e560820000cf5b2148b96b5d0885fc46f1f1
Liam <liamlwrnce@gmail.com>
2022-02-07 16:12:59 +0000

    added function to display all pokemon names and stats

--- functions.php
+++ functions.php
@@ -1,1 +1,12 @@
 <?php
+function showPokemon(array $allPokemon) {
+    $returnedHTML = "";
+    foreach($allPokemon as $pokemon) {
+        $returnedHTML .= "<p>" . "Pokedex ID: " . $pokemon['pokedex_id'] . "</p>" .
+          "<p>" . "Name: " . $pokemon['name'] . "</p>" .
+         "<p>" . "Type 1: " . $pokemon['type_1'] . "</p>" .
+         "<p>" . "Type 2: " . $pokemon['type_2'] . "</p>" .
+         "<p>" . "HP: " . $pokemon['hp'] . "</p>" . "<br>";
+    }
+    return $returnedHTML;
+}
--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once './functions.php';
 $db = new PDO('mysql:host=db; dbname=kantopokemon', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 $query = $db->prepare('SELECT * FROM `kanto`');
@@ -7,11 +7,15 @@ $query->execute();
 $allPokemon = $query->fetchAll();
 //print_r($allPokemon);
 
-foreach ($allPokemon as $pokemon) {
-    echo "<p>" . "Pokedex ID:" . $pokemon['pokedex_id'] . "</p>";
-    echo "<p>" . "Name:" . $pokemon['name'] . "</p>";
-    echo "<p>" . "Type 1:" . $pokemon['type_1'] . "</p>";
-    echo "<p>" . "Type 2:" . $pokemon['type_2'] . "</p>";
-    echo "<p>" . "HP:" . $pokemon['hp'] . "</p>" . "<br>";
-}
+echo "<h1>" . "Pokemon of the Kanto Region" . "</h1>";
+
+echo showPokemon($allPokemon);
+
+//foreach ($allPokemon as $pokemon) {
+//    echo "<p>" . "Pokedex ID: " . $pokemon['pokedex_id'] . "</p>";
+//    echo "<p>" . "Name: " . $pokemon['name'] . "</p>";
+//    echo "<p>" . "Type 1: " . $pokemon['type_1'] . "</p>";
+//    echo "<p>" . "Type 2: " . $pokemon['type_2'] . "</p>";
+//    echo "<p>" . "HP: " . $pokemon['hp'] . "</p>" . "<br>";
+//}
 
