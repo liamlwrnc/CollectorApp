d295a18a1ac43fe19f14ddfff9e21ee38a7f0a1e
Liam <liamlwrnce@gmail.com>
2022-02-08 16:45:27 +0000

    made changes based off trainer pr

--- functions.php
+++ functions.php
@@ -1,15 +1,16 @@
 <?php
 function showPokemon(array $allPokemon) {
     $returnedHTML = "";
+    // inbuilt PHP count() function
     if (count($allPokemon) === count($allPokemon, COUNT_RECURSIVE))  {
         return 'WRONG INPUT';
     }
     foreach($allPokemon as $pokemon) {
-        $returnedHTML .= "<p>" . "Pokedex ID: " . $pokemon['pokedex_id'] . "</p>" .
-          "<p>" . "Name: " . $pokemon['name'] . "</p>" .
-         "<p>" . "Type 1: " . $pokemon['type_1'] . "</p>" .
-         "<p>" . "Type 2: " . $pokemon['type_2'] . "</p>" .
-         "<p>" . "HP: " . $pokemon['hp'] . "</p>" . "<br>";
+        $returnedHTML .= "<p>Pokedex ID: " . $pokemon['pokedex_id'] . "</p>" .
+         "<p>Name: " . $pokemon['name'] . "</p>" .
+         "<p>Type 1: " . $pokemon['type_1'] . "</p>" .
+         "<p>Type 2: " . $pokemon['type_2'] . "</p>" .
+         "<p>HP: " . $pokemon['hp'] . "</p>" . "<br>";
     }
     return $returnedHTML;
 }
--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 require_once './functions.php';
 $db = new PDO('mysql:host=db; dbname=kantopokemon', 'root', 'password');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-$query = $db->prepare('SELECT * FROM `kanto`');
+$query = $db->prepare('SELECT * FROM `kanto pokemon plural`');
 $query->execute();
 $allPokemon = $query->fetchAll();
 
-echo "<h1>" . "Pokemon of the Kanto Region" . "</h1>";
+echo "<h1>Pokemon of the Kanto Region</h1>";
 
 echo showPokemon($allPokemon);
 
