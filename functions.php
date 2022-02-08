608ded10275fbd718b0c1b2dd35432216f80141f
Liam <liamlwrnce@gmail.com>
2022-02-08 13:44:42 +0000

    added form and started styling

--- functions.php
+++ functions.php
@@ -5,11 +5,11 @@ function showPokemon(array $allPokemon) {
         return 'WRONG INPUT';
     }
     foreach($allPokemon as $pokemon) {
-        $returnedHTML .= "<p>" . "Pokedex ID: " . $pokemon['pokedex_id'] . "</p>" .
-          "<p>" . "Name: " . $pokemon['name'] . "</p>" .
-         "<p>" . "Type 1: " . $pokemon['type_1'] . "</p>" .
-         "<p>" . "Type 2: " . $pokemon['type_2'] . "</p>" .
-         "<p>" . "HP: " . $pokemon['hp'] . "</p>" . "<br>";
+        $returnedHTML .= "<div class='pokedexid'><p>" . "Pokedex ID: " . $pokemon['pokedex_id'] . "</p></div>" .
+          "<div class='name'><p>" . "Name: " . $pokemon['name'] . "</p></div>" .
+         "<div class='type1'><p>" . "Type 1: " . $pokemon['type_1'] . "</p></div>" .
+         "<div class='type2'><p>" . "Type 2: " . $pokemon['type_2'] . "</p></div>" .
+         "<div class='hp'><p>" . "HP: " . $pokemon['hp'] . "</p></div>" . "<br>";
     }
     return $returnedHTML;
 }
--- index.php
+++ index.php
@@ -7,6 +7,21 @@ $query->execute();
 $allPokemon = $query->fetchAll();
 
 echo "<h1>" . "Pokemon of the Kanto Region" . "</h1>";
+?>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+</head>
+    <form action="form.php" method="post">
+    Pokedex ID: <input type="text" name="pokedex_id"><br>
+    Name: <input type="text" name="name"><br>
+    Type 1: <input type="text" name="type1"><br>
+    Type 2: <input type="text" name="type2"><br>
+    <input type="submit">
+</form>
 
+</html>
+
+<?php
 echo showPokemon($allPokemon);
 
