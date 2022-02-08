754fc4fd3f99f7af944fb474fd68dae9760abda2
Liam <liamlwrnce@gmail.com>
2022-02-08 14:12:47 +0000

    small styling tweaks

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ echo "<h1>" . "Pokemon of the Kanto Region" . "</h1>";
 <head>
     <link rel="stylesheet" type="text/css" href="stylesheet.css">
 </head>
-    <form action="form.php" method="post">
+    <form class="searchForm" action="form.php" method="post">
     Pokedex ID: <input type="text" name="pokedex_id"><br>
     Name: <input type="text" name="name"><br>
     Type 1: <input type="text" name="type1"><br>
